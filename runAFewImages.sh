7c8fd07042b4f86a8bd491078b2603f7ebecd73c
Colm Carew <colm.carew@distilled.ie>
2017-10-15 14:24:12 +0100

    Adding a script to start a docker image

---
+++ runAFewImages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#docker run -p 8080:8080 -t cachingrest:0.0.1-SNAPSHOT
+#docker run -p 8081:8080 -t cachingrest:0.0.1-SNAPSHOT
+docker run -p 8082:8080 -t cachingrest:0.0.1-SNAPSHOT
