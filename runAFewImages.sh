eb1a35ba6228c74400a00361b241800aad51afd4
Colm Carew <colm.carew@distilled.ie>
2017-10-15 14:55:27 +0100

    some docker compose stuff

--- runAFewImages.sh
+++ runAFewImages.sh
@@ -3,3 +3,6 @@
 #docker run -p 8080:8080 -t cachingrest:0.0.1-SNAPSHOT
 #docker run -p 8081:8080 -t cachingrest:0.0.1-SNAPSHOT
 docker run -p 8082:8080 -t cachingrest:0.0.1-SNAPSHOT
+
+#Get into psql docker image
+#docker exec -it 46e3073988e0  su postgres -c 'psql'
