47737c0918c27038fc833f897e2d4428e702d302
Colm Carew <colm.carew@distilled.ie>
2017-10-17 22:55:08 +0100

    Adding a BigDecimalMath library + a complexMaths endpoint to simulate life without cache on long requests

--- scripts/checkDemoRestApi.sh
+++ scripts/checkDemoRestApi.sh
@@ -5,3 +5,10 @@ do
 	 curl -o /dev/null -s -w %{time_total}\\n -H 'Cache-Control: no-cache' "http://localhost:8080/api/v1/demo/2?dummyquery$i"
 	 sleep 1
 done
+
+
+# Bernouli is 0 for odd numbers
+for i in {100..150}
+do
+	 curl -o /dev/null -s -w %{time_total}\\n -H 'Cache-Control: no-cache' "http://localhost:8080/api/v1/complexMaths/$i"
+done
--- src/main/java/com/colm/cachetest/cachingrest/controller/api/v1/DemoRestController.java
+++ src/main/java/com/colm/cachetest/cachingrest/controller/api/v1/DemoRestController.java
@@ -1,6 +1,7 @@
 package com.colm.cachetest.cachingrest.controller.api.v1;
 
 
+import ch.obermuhlner.math.big.BigDecimalMath;
 import com.colm.cachetest.cachingrest.model.DemoObject;
 import com.colm.cachetest.cachingrest.repository.DemoRepository;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.List;
 
 @RestController
@@ -29,6 +32,18 @@ public class DemoRestController {
         log.info("Finding All Demo Objects");
 
         return demoRepository.findAll();
+    }
+
+    @RequestMapping (value = "/complexMaths/{someNumber}", method = RequestMethod.GET)
+    public BigDecimal doSomeComplexMaths (@PathVariable int someNumber) {
+        log.info("Crunchin the Numbers");
+        MathContext mathContext = new MathContext(100);
+        // Some Dummy Logic
+        int numberToCalculate = someNumber;
+        if(numberToCalculate < 1 || numberToCalculate > 150 ){
+            numberToCalculate = 100;
+        }
+        return BigDecimalMath.bernoulli(numberToCalculate, mathContext);
     }
 
     @Cacheable (value = "post-single", key = "#id", unless = "#result.shares < 10")
