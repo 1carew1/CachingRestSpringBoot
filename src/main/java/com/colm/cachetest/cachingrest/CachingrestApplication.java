2e94154cce2570bd3f3bf371bc606d1e566699aa
Colm Carew <colm.carew@distilled.ie>
2017-10-17 23:21:58 +0100

    Trying to set cache to be no Op - issue is that external library such as the math one may be using its own type of cache

--- src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
+++ src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
@@ -2,9 +2,20 @@ package com.colm.cachetest.cachingrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class CachingrestApplication {
+
+	@Primary
+	@Bean (name = "cacheManager")
+	public CacheManager cacheManager() {
+		return new NoOpCacheManager();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CachingrestApplication.class, args);
