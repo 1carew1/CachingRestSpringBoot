3ad395d646557837d0e16ef16680a88cbf7540d3
Colm Carew <colm.carew@distilled.ie>
2017-10-24 17:03:49 +0100

    Doing up some numbers on how many cubes can be stored in Redis at a time + setting the replacement Algorithm

--- src/main/java/com/colm/cachetest/cachingrest/repository/DatabaseLoader.java
+++ src/main/java/com/colm/cachetest/cachingrest/repository/DatabaseLoader.java
@@ -1,6 +1,9 @@
 package com.colm.cachetest.cachingrest.repository;
 
+import com.colm.cachetest.cachingrest.controller.api.v1.CubeInfoRestController;
 import com.colm.cachetest.cachingrest.service.CubedCacheService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,7 @@ import java.math.BigDecimal;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(DatabaseLoader.class);
 
     @Autowired
     private CubedCacheService cubedCacheService;
@@ -23,9 +27,10 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run (String... string) throws Exception {
-        for (int i = 1; i <= 1200; i++) {
-            long number =  i;
+        for (int i = 1; i <= 27000; i++) {
+            long number = i;
             BigDecimal bigDecimal = BigDecimal.valueOf(number * number * number);
+//            log.info("Trying to put cube into cache : " + number + ", cube = " + bigDecimal);
             cubedCacheService.storeAndCacheCube(number, bigDecimal);
         }
     }
