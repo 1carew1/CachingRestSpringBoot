89ff4edf6bc87b25bb10af5303a9aa79e02c1ddc
Colm Carew <colm.carew@distilled.ie>
2017-11-02 14:35:41 +0000

    Trying to add https://github.com/florind/inception-serving-sb into project

--- src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
+++ src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
@@ -1,19 +1,62 @@
 package com.colm.cachetest.cachingrest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.tensorflow.Graph;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @SpringBootApplication
 @EnableCaching
 public class CachingrestApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(CachingrestApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CachingrestApplication.class, args);
+	}
+
+	@Bean
+	public Graph tfModelGraph(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) throws IOException {
+		Resource graphResource = getResource(tfFrozenModelPath);
+
+		Graph graph = new Graph();
+		graph.importGraphDef(IOUtils.toByteArray(graphResource.getInputStream()));
+		log.info("Loaded Tensorflow model");
+		return graph;
+	}
+
+	private Resource getResource(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) {
+		Resource graphResource = new FileSystemResource(tfFrozenModelPath);
+		if(!graphResource.exists()) {
+			graphResource = new ClassPathResource(tfFrozenModelPath);
+		}
+		if(!graphResource.exists()) {
+			throw new IllegalArgumentException(String.format("File %s does not exist", tfFrozenModelPath));
+		}
+		return graphResource;
+	}
+
+	@Bean
+	public List<String> tfModelLabels(@Value("${tf.labelsPath}") String labelsPath) throws IOException {
+		Resource labelsRes = getResource(labelsPath);
+		log.info("Loaded model labels");
+		return IOUtils.readLines(labelsRes.getInputStream(), Charset.forName("UTF-8")).stream()
+				.map(label -> label.substring(label.contains(":") ? label.indexOf(":")+1 : 0)).collect(Collectors.toList());
 	}
 }
--- src/main/java/com/colm/cachetest/cachingrest/controller/api/v1/CachingRestController.java
+++ src/main/java/com/colm/cachetest/cachingrest/controller/api/v1/CachingRestController.java
@@ -3,15 +3,18 @@ package com.colm.cachetest.cachingrest.controller.api.v1;
 
 import com.colm.cachetest.cachingrest.model.CubedInfo;
 import com.colm.cachetest.cachingrest.model.FactorialInfo;
+import com.colm.cachetest.cachingrest.service.ClassifyImageService;
 import com.colm.cachetest.cachingrest.service.CubedCacheService;
 import com.colm.cachetest.cachingrest.service.FactorialCacheService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicMatch;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping ("/api/v1")
@@ -23,6 +26,8 @@ public class CachingRestController {
     private CubedCacheService cubedCacheService;
     @Autowired
     private FactorialCacheService factorialCacheService;
+    @Autowired
+    private ClassifyImageService classifyImageService;
 
     @RequestMapping (value = "/cube/{someNumber}", method = RequestMethod.GET)
     public CubedInfo doSomeComplexMaths (@PathVariable Long someNumber) {
@@ -38,5 +43,30 @@ public class CachingRestController {
         FactorialInfo factorialInfo = factorialCacheService.computerFactorial(someNumber);
         log.info(someNumber + "! = " + factorialInfo.getFactorial().toString());
         return factorialInfo;
+    }
+
+    @PostMapping (value = "/classify")
+    @CrossOrigin(origins = "*")
+    public ClassifyImageService.LabelWithProbability classifyImage(@RequestParam MultipartFile file) throws IOException {
+        checkImageContents(file);
+        return classifyImageService.classifyImage(file.getBytes());
+    }
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    private void checkImageContents(MultipartFile file) {
+        MagicMatch match;
+        try {
+            match = Magic.getMagicMatch(file.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String mimeType = match.getMimeType();
+        if (!mimeType.startsWith("image")) {
+            throw new IllegalArgumentException("Not an image type: " + mimeType);
+        }
     }
 }
