ffa2b1845c18ca98795203ba7092129fc7b7a6bd
Colm Carew <colmcarew2@gmail.com>
2018-02-20 21:33:00 +0000

    Removing unsed file

--- runAFewImages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#docker run -p 8080:8080 -t cachingrest:0.0.1-SNAPSHOT
-#docker run -p 8081:8080 -t cachingrest:0.0.1-SNAPSHOT
-docker run -p 8082:8080 -t cachingrest:0.0.1-SNAPSHOT
-
-#Get into psql docker image
-#docker exec -it 46e3073988e0  su postgres -c 'psql'
