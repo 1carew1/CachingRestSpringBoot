07bb85a94f4c147bcc40732058680e392cedebc8
Colm Carew <colmcarew2@gmail.com>
2018-03-15 21:20:39 +0000

    Adding in the comments for Couchbase

--- src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
+++ src/main/java/com/colm/cachetest/cachingrest/CachingrestApplication.java
@@ -1,5 +1,10 @@
 package com.colm.cachetest.cachingrest;
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.spring.cache.CacheBuilder;
+import com.couchbase.client.spring.cache.CouchbaseCacheManager;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
@@ -25,39 +31,59 @@ import java.util.stream.Collectors;
 @EnableCaching
 public class CachingrestApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(CachingrestApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(CachingrestApplication.class);
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(CachingrestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CachingrestApplication.class, args);
+    }
 
-	@Bean
-	public Graph tfModelGraph(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) throws IOException {
-		Resource graphResource = getResource(tfFrozenModelPath);
+    @Bean
+    public Graph tfModelGraph(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) throws IOException {
+        Resource graphResource = getResource(tfFrozenModelPath);
 
-		Graph graph = new Graph();
-		graph.importGraphDef(IOUtils.toByteArray(graphResource.getInputStream()));
-		log.info("Loaded Tensorflow model");
-		return graph;
-	}
+        Graph graph = new Graph();
+        graph.importGraphDef(IOUtils.toByteArray(graphResource.getInputStream()));
+        log.info("Loaded Tensorflow model");
+        return graph;
+    }
 
-	private Resource getResource(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) {
-		Resource graphResource = new FileSystemResource(tfFrozenModelPath);
-		if(!graphResource.exists()) {
-			graphResource = new ClassPathResource(tfFrozenModelPath);
-		}
-		if(!graphResource.exists()) {
-			throw new IllegalArgumentException(String.format("File %s does not exist", tfFrozenModelPath));
-		}
-		return graphResource;
-	}
+    private Resource getResource(@Value("${tf.frozenModelPath}") String tfFrozenModelPath) {
+        Resource graphResource = new FileSystemResource(tfFrozenModelPath);
+        if (!graphResource.exists()) {
+            graphResource = new ClassPathResource(tfFrozenModelPath);
+        }
+        if (!graphResource.exists()) {
+            throw new IllegalArgumentException(String.format("File %s does not exist", tfFrozenModelPath));
+        }
+        return graphResource;
+    }
 
-	@Bean
-	public List<String> tfModelLabels(@Value("${tf.labelsPath}") String labelsPath) throws IOException {
-		Resource labelsRes = getResource(labelsPath);
-		log.info("Loaded model labels");
-		return IOUtils.readLines(labelsRes.getInputStream(), Charset.forName("UTF-8")).stream()
-				.map(label -> label.substring(label.contains(":") ? label.indexOf(":")+1 : 0)).collect(Collectors.toList());
-	}
+    @Bean
+    public List<String> tfModelLabels(@Value("${tf.labelsPath}") String labelsPath) throws IOException {
+        Resource labelsRes = getResource(labelsPath);
+        log.info("Loaded model labels");
+        return IOUtils.readLines(labelsRes.getInputStream(), Charset.forName("UTF-8")).stream()
+                .map(label -> label.substring(label.contains(":") ? label.indexOf(":") + 1 : 0)).collect(Collectors.toList());
+    }
+
+
+    // Code for getting working with Couchbase
+//    @Bean(destroyMethod = "disconnect")
+//    public Cluster cluster() {
+//        // connect to the couchbase-server running on your local machine
+//        return CouchbaseCluster.create();
+//    }
+//
+//    @Bean(destroyMethod = "close")
+//    public Bucket bucket() {
+//        cluster().authenticate("colm", "password");
+//        return cluster().openBucket("colmcachetest");
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager() {
+//        CacheBuilder cacheBuilder = CacheBuilder.newInstance(bucket()).withExpiration(0);
+//        return new CouchbaseCacheManager(cacheBuilder, "imageClassifications");
+//    }
 }
